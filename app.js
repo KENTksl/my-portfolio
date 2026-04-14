45cb61598373ed7b236d2ea2c5b5e2492e26b0ee
qminh <133646909+KENTksl@users.noreply.github.com>
2026-04-14 09:46:14 +0700

    chore: add app.js startup for hosting

---
+++ app.js
@@ -0,0 +1,20 @@
+const http = require("http");
+const next = require("next");
+
+const port = Number.parseInt(process.env.PORT || "3000", 10);
+const hostname = process.env.HOSTNAME || "0.0.0.0";
+
+const app = next({ dev: false, hostname, port });
+const handle = app.getRequestHandler();
+
+app
+  .prepare()
+  .then(() => {
+    http
+      .createServer((req, res) => handle(req, res))
+      .listen(port, hostname, () => {});
+  })
+  .catch(() => {
+    process.exit(1);
+  });
+
